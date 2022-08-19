34e1b62edd4398061893c1e21b44095beb027451
Louie Moretti <morettilouie@gmail.com>
2022-08-18 19:06:13 -0500

    updated seeds

--- seeds/post-seeds.js
+++ seeds/post-seeds.js
@@ -2,104 +2,103 @@ const { Post } = require('../models');
 
 const postdata = [
   {
-    title: 'Donec posuere metus vitae ipsum.',
-    post_url: 'https://buzzfeed.com/in/imperdiet/et/commodo/vulputate.png',
+    title: 'Transformers',
+    content: 'Optimus Prime is my hero!',
     user_id: 10
   },
   {
-    title: 'Morbi non quam nec dui luctus rutrum.',
-    post_url: 'https://nasa.gov/donec.json',
+    title: 'Blink182',
+    content: 'This is the best band ever',
     user_id: 8
   },
   {
-    title: 'Donec diam neque, vestibulum eget, vulputate ut, ultrices vel, augue.',
-    post_url: 'https://europa.eu/parturient/montes/nascetur/ridiculus/mus/etiam/vel.aspx',
+    title: 'Pineapple on pizza',
+    content: "I'd rather starve to death",
     user_id: 1
-  },
-  {
+  },{
     title: 'Nunc purus.',
-    post_url: 'http://desdev.cn/enim/blandit/mi.jpg',
+    content: 'desdev.cn/enim/blandit/mi',
     user_id: 4
   },
   {
     title: 'Pellentesque eget nunc.',
-    post_url: 'http://google.ca/nam/nulla/integer.aspx',
+    content: 'google.ca/nam/nulla/integer',
     user_id: 7
   },
   {
     title: 'Lorem ipsum dolor sit amet, consectetuer adipiscing elit.',
-    post_url: 'https://stanford.edu/consequat.png',
+    content: 'stanford.edu/consequat',
     user_id: 4
   },
   {
     title: 'In hac habitasse platea dictumst.',
-    post_url: 'http://edublogs.org/non/ligula/pellentesque.js',
+    content: 'edublogs.org/non/ligula/pellentesque',
     user_id: 1
   },
   {
     title: 'Morbi non quam nec dui luctus rutrum.',
-    post_url: 'http://ucla.edu/consequat/nulla.html',
+    content: 'ucla.edu/consequat/nulla.html',
     user_id: 1
   },
   {
     title: 'Duis ac nibh.',
-    post_url: 'http://theguardian.com/dui/vel/nisl/duis/ac/nibh.aspx',
+    content: 'theguardian.com/dui/vel/nisl/duis/ac/nibh',
     user_id: 9
   },
   {
     title: 'Curabitur at ipsum ac tellus semper interdum.',
-    post_url: 'https://reverbnation.com/ligula/sit.jpg',
+    content: 'reverbnation.com/ligula/sit.jpg',
     user_id: 5
   },
   {
     title: 'In hac habitasse platea dictumst.',
-    post_url: 'http://china.com.cn/lectus/vestibulum.json',
+    content: 'china.com.cn/lectus/vestibulumon',
     user_id: 3
   },
   {
     title: 'Morbi odio odio, elementum eu, interdum eu, tincidunt in, leo.',
-    post_url: 'http://networksolutions.com/nam/ultrices/libero/non/mattis/pulvinar.json',
+    content: 'networksolutions.com/nam/ultrices/libero/non/mattis/pulvinaron',
     user_id: 10
   },
   {
     title: 'Donec dapibus.',
-    post_url: 'https://instagram.com/ac/neque/duis/bibendum/morbi/non.xml',
+    content: 'instagram.com/ac/neque/duis/bibendum/morbi/non.xml',
     user_id: 8
   },
   {
     title: 'Nulla tellus.',
-    post_url: 'https://lycos.com/natoque/penatibus/et.html',
+    content: 'lycos.com/natoque/penatibus/et.html',
     user_id: 3
   },
   {
     title: 'Cras mi pede, malesuada in, imperdiet et, commodo vulputate, justo.',
-    post_url: 'https://gmpg.org/lorem.jpg',
+    content: 'gmpg.org/lorem.jpg',
     user_id: 3
   },
   {
     title:
       'Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Mauris viverra diam vitae quam.',
-    post_url: 'https://paginegialle.it/mattis/egestas.jsp',
+    content: 'paginegialle.it/mattis/egestasp',
     user_id: 7
   },
   {
     title: 'In hac habitasse platea dictumst.',
-    post_url: 'http://wikia.com/turpis/eget.jpg',
+    content: 'wikia.com/turpis/eget.jpg',
     user_id: 6
   },
   {
     title: 'Etiam justo.',
-    post_url: 'https://shareasale.com/quis.json',
+    content: 'shareasale.com/quison',
     user_id: 4
   },
   {
     title: 'Nulla ut erat id mauris vulputate elementum.',
-    post_url: 'http://java.com/diam/neque/vestibulum/eget/vulputate/ut/ultrices.png',
+    content: 'java.com/diam/neque/vestibulum/eget/vulputate/ut/ultrices',
     user_id: 6
   },
   {
     title: 'Integer pede justo, lacinia eget, tincidunt eget, tempus vel, pede.',
-    post_url: 'https://java.com/at/nibh/in.png',
+    content: 'java.com/at/nibh/in',
     user_id: 7
   }
 ];
